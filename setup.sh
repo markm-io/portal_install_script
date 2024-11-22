6570f37ef89fbc8b9bd62ba8d5d66300bcc95d05
markm-io <mark@securehst.com>
2024-11-21 21:42:58 -0600

    setup.sh initial commit

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 
 # Determine the portal folder based on the available paths
 if [ -d "/mnt/host/c" ]; then
-    portal_folder="/mnt/host/c/Vista/Portal"
+    postgres_folder="/mnt/host/c/Vista/Portal"
 elif [ -d "/mnt/c" ]; then
-    portal_folder="/mnt/c/Vista/Portal"
+    postgres_folder="/mnt/c/Vista/Portal"
 else
-    portal_folder="/opt/Vista/Portal"
+    postgres_folder="/opt/Vista/Portal"
 fi
 
 # Ensure the portal folder exists
-if [ ! -d "$portal_folder" ]; then
-    echo "Creating portal folder: $portal_folder"
-    mkdir -p "$portal_folder"
+if [ ! -d "$postgres_folder" ]; then
+    echo "Creating portal folder: $postgres_folder"
+    mkdir -p "$postgres_folder"
 fi
 
 # Define the URL of the script to be downloaded
 SCRIPT_URL="https://raw.githubusercontent.com/markm-io/portal_install_script/main/install.sh"
-LOCAL_FILE="$portal_folder/install.sh"
+LOCAL_FILE="$postgres_folder/install.sh"
 
 echo "Downloading the installation script..."
 # Download the script
